77260992ade16f92286b949f86f54897d71cdd14
Chaitanya DA <58428344+chaitanya371@users.noreply.github.com>
2021-07-28 09:54:03 +0530

    Create filetype.sh

---
+++ filetype.sh
@@ -0,0 +1,29 @@
+$ cat filetype.sh
+#!/bin/bash
+
+for filename in $(ls)
+do
+	# Take extension available in a filename
+	ext=${filename##*\.}
+	case "$ext" in
+        c) echo "$filename : C source file"
+           ;;
+        o) echo "$filename : Object file"
+           ;;
+        sh) echo "$filename : Shell script"
+            ;;
+        txt) echo "$filename : Text file"
+        	 ;;
+        *) echo " $filename : Not processed"
+           ;;
+	esac
+done
+
+$ ./filetype.sh
+a.c : C source file
+b.c : C source file
+c1.txt : Text file
+fileop.sh : Shell script
+obj.o : Object file
+text : Not processed
+t.o : Object file
